6ccb337de8cf0e8f2018829ec3e3046aaba2e5ef
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-03-24 11:32:27 +0530

    added service and repo

--- src/main/java/com/blobProj/abhiJ/abhinandanJoshi/Blog/Repository/BlogRepository.java
+++ src/main/java/com/blobProj/abhiJ/abhinandanJoshi/Blog/Repository/BlogRepository.java
@@ -1,4 +1,6 @@
 package com.blobProj.abhiJ.abhinandanJoshi.Blog.Repository;
+import com.blobProj.abhiJ.abhinandanJoshi.Blog.Model.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class BlogRepository {
+public interface BlogRepository extends JpaRepository<Blog, Long> {
 }
--- src/main/java/com/blobProj/abhiJ/abhinandanJoshi/Blog/Services/BlogService.java
+++ src/main/java/com/blobProj/abhiJ/abhinandanJoshi/Blog/Services/BlogService.java
@@ -1,4 +1,46 @@
 package com.blobProj.abhiJ.abhinandanJoshi.Blog.Services;
 
+import com.blobProj.abhiJ.abhinandanJoshi.Blog.Model.Blog;
+import com.blobProj.abhiJ.abhinandanJoshi.Blog.Repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
 public class BlogService {
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+    public List<Blog> getAllBlogs() {
+        return blogRepository.findAll();
+    }
+
+    public Blog getBlogById(Long id) {
+        return blogRepository.findById(id).orElse(null);
+    }
+
+    public Blog addBlog(Blog blog) {
+        blog.setCreatedAt(LocalDateTime.now());
+        return blogRepository.save(blog);
+    }
+
+    public Blog updateBlog(Long id, Blog blog) {
+        if (blogRepository.existsById(id)) {
+            blog.setId(id);
+            return blogRepository.save(blog);
+        }
+        return null;
+    }
+
+    public boolean deleteBlog(Long id) {
+        if (blogRepository.existsById(id)) {
+            blogRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
+
